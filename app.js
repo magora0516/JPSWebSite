e9680bb3dfde42f933fdf4882420452b2bc52a0e
magora0516 <80123821+magora0516@users.noreply.github.com>
2025-08-10 12:35:00 -0500

    vis19

--- app.js
+++ app.js
@@ -333,7 +333,7 @@ function startCountdownIfPlanned(){
     ((s.worker_id && a.worker_id && s.worker_id === a.worker_id) ||
      (!s.worker_id && s.worker && a.worker && s.worker.toLowerCase() === a.worker.toLowerCase()))
   ) */
-    console.log('Intento de conteo',a.date, a.client_id, plan.client_id)
+    console.log('Intento de conteo',a.date, a.client_id)
   if (!plan) { $('#countdown').textContent = '00:00:00'; return }
   console.log('Se encontró plan:', plan.client_id)
   const endTarget = new Date(a.start_at).getTime() + plan.minutes * 60000
