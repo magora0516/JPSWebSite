855e471a6477bdd25fb765beacc600fe8a2c41cc
magora0516 <80123821+magora0516@users.noreply.github.com>
2025-09-07 08:18:57 -0500

    Descomentado

--- sessions.js
+++ sessions.js
@@ -223,15 +223,15 @@ function bindEvents() {
 async function init() {
     console.log('Sessions admin init')
     await refreshSession()
-    //const [workers, clients] = await Promise.all([fetchWorkers(), fetchClients()])
-    //state.workers = workers; state.clients = clients
-    //fillSelects()
+    const [workers, clients] = await Promise.all([fetchWorkers(), fetchClients()])
+    state.workers = workers; state.clients = clients
+    fillSelects()
 
     // precarga: hoy
-    //$('#fFrom').value = todayStr()
-    //$('#fTo').value = todayStr()
-    //state.rows = await fetchSessionsRange(todayStr(), todayStr(), null, null)
-    //renderTable()
+    $('#fFrom').value = todayStr()
+    $('#fTo').value = todayStr()
+    state.rows = await fetchSessionsRange(todayStr(), todayStr(), null, null)
+    renderTable()
 }
 
 bindEvents()
