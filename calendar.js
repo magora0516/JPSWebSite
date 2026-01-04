0a8ac36918597b2a8da50f7679d9dfc655711dfa
magora0516 <80123821+magora0516@users.noreply.github.com>
2026-01-04 01:15:51 -0500

    Corrección 3

--- calendar.js
+++ calendar.js
@@ -10,6 +10,7 @@ const fmtYmd = d => {
     return `${y}-${m}-${day}`
 }
 
+// corregido
 function ymdLocal(d){
     const y = d.getFullYear()
     const m = String(d.getMonth() + 1).padStart(2, '0')
